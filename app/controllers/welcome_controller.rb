b4ffd6669c1ecb48cdeebe92f4e4c15b5da33784
Guilherme Souza <guivideojob@gmail.com>
2012-10-13 14:24:14 -0300

    if not error

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    graph = Koala::Facebook::GraphAPI.new(current_user.token)
-    @checkins = graph.fql_query("SELECT author_uid, checkin_id, tagged_uids, page_id, timestamp FROM checkin WHERE author_uid in (SELECT uid2 FROM friend WHERE uid1 = me())")
-
+    if current_user
+      graph = Koala::Facebook::GraphAPI.new(current_user.token)
+      @checkins = graph.fql_query("SELECT author_uid, checkin_id, tagged_uids, page_id, timestamp FROM checkin WHERE author_uid in (SELECT uid2 FROM friend WHERE uid1 = me())")
+    end
   end
 end
