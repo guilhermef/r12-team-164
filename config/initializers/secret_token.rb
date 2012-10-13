1a0738b48f952543a943eb58ac4aced80e306f38
Guilherme Souza <guivideojob@gmail.com>
2012-10-12 21:28:14 -0300

    it's time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialDrink::Application.config.secret_token = '8872c6611e5d33342c34956307e3446c2a5ee3b86f258cd0679d9fc898a27ec984625a4204bf05fd139ce0ddf9fe93b7bcc838a0f02b623b57241c2c9f0d9fc0'
