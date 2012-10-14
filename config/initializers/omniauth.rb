50c4e61682416084bcfb6532a826f1eda23294f7
Guilherme Souza <guivideojob@gmail.com>
2012-10-13 21:10:46 -0300

    fixing perms

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :facebook, '411973878869082', 'ed5b297c305d205d01a93cf6d16ffd3a', {:scope => 'email, read_friendlists, user_checkins, friends_checkins'}
+  provider :facebook, '411973878869082', 'ed5b297c305d205d01a93cf6d16ffd3a', {:scope => 'email, read_friendlists, user_checkins, friends_checkins, user_status, friends_status'}
 
 end
